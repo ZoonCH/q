341a20d175319234a7cd57e13309d5e95f8b5772
ZoonCH <108607159+ZoonCH@users.noreply.github.com>
2022-08-07 17:10:01 +0530

    Add files via upload

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,11 +5,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `Invite Group
-• 15 Day / Rp 5k
-• 30 Day / Rp 10k
-
-Jika berminat hubungi: @${global.owner[0]} untuk order:)
+    let teks = `Untuk Invite Bot ke grup ketik #sewa atau hubungi owner kami
 `
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
